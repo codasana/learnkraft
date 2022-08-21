b52ad416a72ee9cbc9b680f3c87b4fd61af33cfd
Manish <msahajwani@gmail.com>
2022-08-21 09:40:29 +0530

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: process.env.SITE_URL || 'https://learnkraft.com/',
+    siteUrl: 'https://learnkraft.com',
     generateRobotsTxt: true, // (optional)
-    // ...other options
   }
